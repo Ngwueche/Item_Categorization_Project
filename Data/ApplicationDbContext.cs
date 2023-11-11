b98dc741761e0d6cddde3608aa846fdb69257fad
ngwueche <wisdomngwueche@gmail.com>
2023-11-11 15:35:35 +0100

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_Project.Models;
+
+namespace MVC_Project.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
